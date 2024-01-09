47f8dc9370cce851bcbe2f27f2a888332e32e8d9
Isaac Chan <129546661+IsaacChan721@users.noreply.github.com>
2024-01-08 22:45:32 -0500

    Add files via upload

---
+++ RandomNumberGenerator.py
@@ -0,0 +1,11 @@
+import time
+
+# try:
+#     low_bound = input("Enter a lower bound number. ")
+#     upper_bound = input("Enter an upper bound number. ")
+
+# except:
+#     print("Invalid Input! Try again.")
+
+num = time.time()
+print(num % 1)
